1627be24084a0595a2e46ce9b1879b268ee961d1
SophGits <sophie.jiffy@gmail.com>
2014-08-16 14:40:35 +0100

    Implement matching patterns for strings, numbers, functions and brackets and assign each a class with associated colour.

--- syntax-highlighter/script.js
+++ syntax-highlighter/script.js
@@ -5,7 +5,20 @@ $(document).ready(function(){
   // highlighting special characters: /, *, + are escaped using a backslash
   // g replaces all occurences of the match
   // $1 is a back-reference to the parenthesised part of the match
-  c = c.replace(/(=|%|\/|\*|-|,|;|\+|<|>)/g, "<span class=\|sc\">$1</span>");
+  c = c.replace(/(=|%|\/|\*|-|,|;|\+|<|>)/g, "<span class=\"sc\">$1</span>");
+
+  //  Strings - text inside single quotes and backticks
+  c = c.replace("/(['`].*?['`])/g", "<span class=\"string\">$1</span>");
+
+  //  Numbers - same colour as strings
+  c = c.replace(/(\d+)/g, "<span class=\"string\">$1</span>");
+
+  // Functions - any string followed by a '('
+  c = c.replace(/(\w*?)\(/g, "<span class=\"function\">$1</span>(");
+
+  // Brackets - same as special characters
+  c = c.replace(/([\(\)])/g, "<span class=\"sc\">$1</span>");
+
 
   $("#layer").html(c); // injecting code into <pre></pre>
 })
