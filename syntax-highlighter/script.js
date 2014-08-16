a01f23086d3c803171807517ea3774012b2d59f1
SophGits <sophie.jiffy@gmail.com>
2014-08-16 15:48:59 +0100

    Insert array of keywords and write matches for them (stripping out the quotation marks) and for comments (stripping out existing special character markup).

--- syntax-highlighter/script.js
+++ syntax-highlighter/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+  // reserved mysql.keywords:
+  var k = ["AND", "AS", "ASC", "BETWEEN", "BY", "CASE", "CURRENT_DATE", "CURRENT_TIME", "DELETE", "DESC", "DISTINCT", "EACH", "ELSE", "ELSEIF", "FALSE", "FOR", "FROM", "GROUP", "HAVING", "IF", "IN", "INSERT", "INTERVAL", "INTO", "IS", "JOIN", "KEY", "KEYS", "LEFT", "LIKE", "LIMIT", "MATCH", "NOT", "NULL", "ON", "OPTION", "OR", "ORDER", "OUT", "OUTER", "REPLACE", "RIGHT", "SELECT", "SET", "TABLE", "THEN", "TO", "TRUE", "UPDATE", "VALUES", "WHEN", "WHERE"];
+  var len = k.length;
+  for(var i=0; i<len; i++){
+    k.push(k[i].toLowerCase());
+  }
+
+  var re;
   var c = $("#highlight").val(); // raw code
 
   // regex
@@ -19,6 +27,26 @@ $(document).ready(function(){
   // Brackets - same as special characters
   c = c.replace(/([\(\)])/g, "<span class=\"sc\">$1</span>");
 
+  // Reserved mysql.keywords
+  for(var i=0; i<k.length; i++){
+    // replace function does not accept a string as a regex pattern, so use a regex object:
+    re = new RegExp("\\b"+k[i]+"\\b", "g");
+    c = c.replace(re, "<span class=\"keyword\">"+k[i]+"</span>");
+  }
+
+  //  Comments
+  //  - those starting with a #:
+  c = c.replace(/(#.*?\n)/g, "<span class=\"comment\">$1</span>");
+  // - those starting with '-- '
+  // first, remove spans applied to the '--' as a special character
+  c = c.replace(/<span class=\"sc\">-<\/span><span class=\"\sc\">-<\/span>/g, "--");
+  c = c.replace(/(-- .*?\n)/g, "<span class=\"comment\">$1</span>");
+
+  // - those inside /*...*/
+  // filtering out spans attached to /* and */ as special characters
+  c = c.replace(/<span class=\"sc\">\/<\/span><span class=\"sc\">\*<\/span>/g, "*/");
+  // in JS the dot operator cannot match newlines, so use [\s\S] as a hack to select everything
+  c = c.replace(/(\/\*[\s\S]*?\*\/)/g, "<span class=\"comment\">$1</span>");
 
   $("#layer").html(c); // injecting code into <pre></pre>
 })
