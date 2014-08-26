c65fdb7e313be207ca83398ecc55b1b5c285b4ca
SophGits <sophie.jiffy@gmail.com>
2014-08-26 23:12:42 +0100

    Write regular expressions to match 'milk', 'coconut milk' and variations of 'skimmed milk' and style appropriately.

--- ingredients-match/script.js
+++ ingredients-match/script.js
@@ -3,22 +3,46 @@ window.onload = function(){
 
   // Check ingredient against a regex
   function checkIngredients(items){
+    veganMatches =[];
+    nonVeganMatches = [];
+
     $.each(items, function(index, item){
 
-      // match for "milk"
-      var matchMilk = /(milk)/g.exec(item)
-      if (matchMilk != null){
+        // match for coconut milk
+        var matchCoconutMilk = /\s*coconut\s*milk\s*/g.exec(item)
+        //match for skimmed milk
+        var matchSkimmedMilk = /\s*(skimmed|skim)\s*milk/.exec(item);
+        // match for "milk" on its own
+        var matchMilk = /^\s*milk\s*$/g.exec(item)
+
+        if (matchCoconutMilk != null){
+          var match = matchCoconutMilk["input"];
+          veganMatches.push(match);
+          // console.log(veganMatches)
+        }
+        else if (matchSkimmedMilk != null){
+          var match = matchSkimmedMilk["input"];
+          nonVeganMatches.push(match);
+        }
+      else if (matchMilk != null){
         var match = matchMilk["input"];
-        // eg "milkman" or "milk" or "coconut milk"
+        nonVeganMatches.push(match);
       }
 
     });
+    listIngredients(veganMatches);
+    listNonVeganIngredients(nonVeganMatches);
   }
 
-  // List each ingredient under Results
+  // List each ingredient under Results or Non-vegan
   function listIngredients(items){
     $.each(items, function(index, item){
       $(".results ol").append("<li>" + item + "</li>");
+    });
+  }
+  function listNonVeganIngredients(items){
+    $.each(items, function(index, item){
+      $(".non-vegan-results ol").append("<li>" + item + "</li>");
     });
   }
 
