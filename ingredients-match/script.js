f52cd81748140ed5bba4f9bbdf8b0cfdd41b99fe
SophGits <sophie.jiffy@gmail.com>
2014-08-26 22:00:33 +0100

    Match the occurrence of a specified string using a regular expression and return the input which contains it.

--- ingredients-match/script.js
+++ ingredients-match/script.js
@@ -1,8 +1,22 @@
 window.onload = function(){
 
-  // Lists each ingredient under Results
+
+  // Check ingredient against a regex
   function checkIngredients(items){
-    // console.log(items);
+    $.each(items, function(index, item){
+
+      // match for "milk"
+      var matchMilk = /(milk)/g.exec(item)
+      if (matchMilk != null){
+        var match = matchMilk["input"];
+        // eg "milkman" or "milk" or "coconut milk"
+      }
+
+    });
+  }
+
+  // List each ingredient under Results
+  function listIngredients(items){
     $.each(items, function(index, item){
       $(".results ol").append("<li>" + item + "</li>");
     });
