c7a40208637f1aee77556e5b438240b8f502c205
SophGits <sophie.jiffy@gmail.com>
2014-08-30 13:12:11 +0100

    Add more types of milk and classify as vegan or not.

--- ingredients-match/script.js
+++ ingredients-match/script.js
@@ -9,25 +9,20 @@ window.onload = function(){
     $.each(items, function(index, item){
 
         // match for coconut milk
-        var matchCoconutMilk = /\s*coconut\s*milk\s*/g.exec(item)
+        var matchVeganMilks = /\s*(coconut|soy|soya)\s*milk\s*/g.exec(item)
         //match for skimmed milk
-        var matchSkimmedMilk = /\s*(skimmed|skim)\s*milk/.exec(item);
+        var matchMilks = /\s*(whole|semi-skimmed|semi-skim|skimmed|skim)\s*milk/.exec(item);
         // match for "milk" on its own
         var matchMilk = /^\s*milk\s*$/g.exec(item)
 
-        if (matchCoconutMilk != null){
-          var match = matchCoconutMilk["input"];
+        if (matchVeganMilks != null){
+          var match = matchVeganMilks["input"];
           veganMatches.push(match);
-          // console.log(veganMatches)
         }
-        else if (matchSkimmedMilk != null){
-          var match = matchSkimmedMilk["input"];
+        else if (matchMilks != null){
+          var match = matchMilks["input"] || matchMilk["input"];
           nonVeganMatches.push(match);
         }
-      else if (matchMilk != null){
-        var match = matchMilk["input"];
-        nonVeganMatches.push(match);
-      }
 
     });
     listIngredients(veganMatches);
