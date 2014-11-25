4d0adf4105e7f862a5a7bec41cd6d72f20c791bb
SophGits <sophie.jiffy@gmail.com>
2014-11-25 20:17:35 +0000

    Create the collection view and annotate.

--- backbone_book_tutorial/js/app.js
+++ backbone_book_tutorial/js/app.js
@@ -33,5 +33,35 @@
     bookView = new BookView({
       model: book
     });
+    // BOOK COLLECTION
+    var Library = Backbone.Collection.extend({
+        model:Book
+    });
+    //COLLECTION VIEW
+    var LibraryView = Backbone.View.extend({
+      el: $("#books"),
+      // a view can take a tagName or an el. When using tagName the view will create the element for us (and we are responsibe for inserting it into the page). By using el we are specifying an existing element and the view will write it in where it already is on the page.
+      initialize: function(){
+      // initialize is optional but must contain a function - which will be called by Backbone when a view constructor is called.
+      // the render means as soon as we call the LibraryView constructor it will get rendered, so this is a self rendering view. We don’t have to make it self rendered but it is common practice.
+        this.collection = new Library(books);
+        // Above: Library is a collection that expects an array of objects that it can use to create Book models. We haven’t defined the books variable yet....
+        this.render();
+      },
+      // Below, we are iterating over all the models (Books) in our collection. The first argument to “each” is the array that will be iterated over. The second argument is the function that will be applied to each member of the array. The function in our case calls the renderBook function with the current model as argument. We need to use “that” to get this right since if we would have used “this” it would have referenced the function itself.
+      render: function(){
+        var that = this;
+        _.each(this.collection.models, function(item){
+          that.renderBook(item);
+        }, this); // 'this' is the context it is rendering in
+      },
+      // Next we define renderBook, which takes a model (a Book) as argument and uses it to create a BookView. The bookView is then rendered and appended to the view container as specified in our el property (on line 02 of this fn).
+      renderBook: function(item){
+        var bookView = new BookView({
+          model: item
+        });
+        this.$el.append(bookView.render().el);
+      }
+    })
 
 })(jQuery);
