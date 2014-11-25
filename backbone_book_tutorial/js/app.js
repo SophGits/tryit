b684067233518e90909b0c71b1761135b361c20d
SophGits <sophie.jiffy@gmail.com>
2014-11-25 19:46:00 +0000

    Create a view for the model.

--- backbone_book_tutorial/js/app.js
+++ backbone_book_tutorial/js/app.js
@@ -10,4 +10,17 @@
         }
     });
 
+    var BookView = Backbone.View.extend({
+      tagName: "div",
+      className: "bookContainer",
+      template: $("#bookTemplate").html(),
+
+      render: function(){
+        var tmpl = _.template(this.template); //tmpl is a fn that takes a JSON object and returns HTML
+
+        this.$el.html(tmpl(this.model.toJSON())); //this.el is what we defined in tagName. Use $el to access the jQuery html() fn
+        return this;
+      }
+    });
+
 })(jQuery);
