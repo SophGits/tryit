0fec67e338e665967ac17fe7e96c4fd02025e217
SophGits <sophie.jiffy@gmail.com>
2014-11-27 15:14:31 +0000

    Reinsert rails app as it was not the directory causing the oversize git pack issue.

---
+++ rails_app/toy_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = '42659d7dc6a12b6cca82a9d73da7d552a28883aef34ced05d20f6ae05417b5ac73f0459a13bcfa8933e0985f2543cd3e37b0515edb032d895008a22ddeeb32bf'
