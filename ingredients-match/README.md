a797daf309f0abd4ad62137e50ce33c2f0eb5e79
SophGits <sophie.jiffy@gmail.com>
2014-11-27 14:02:20 +0000

    Remove ingredients project to free up space in this git repo.

--- ingredients-match/README.md
+++
@@ -1,3 +0,0 @@
-This is a pattern-matching practice exercise using regular expressions.
-
-A user can submit a comma-separated list of ingredients to find out which are vegan.
--- ingredients-match/show-results.js
+++
@@ -1,6 +0,0 @@
-function showItems(items, container){
-    container.html('');
-  $.each(items, function(index, item){
-    container.append("<li>" + item + "</li>");
-  });
-}
